acda52a50c004c624ae9e8f825451eef6c7964b1
apollo1860 <167482632+apollo1860@users.noreply.github.com>
2025-09-03 13:39:02 +0200

    script.js aktualisieren

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ const categories = {
     // 2. Zahlenzauber (1 bis 10)
     "2": [
         "Wie oft gehst du am Tag auf die Toilette?",
+        "Wie viele Freunde würden dir beim Umzug helfen?",
         "Auf wie vielen Dates sollte man gewesen sein, bevor man sich das erste Mal küsst?",
         "Wie häufig in der Woche bestellst / gehst du essen?",
         "Mit wie vielen Kleinwüchsigen könntest du es in einem Kampf auf einmal aufnehmen?",
